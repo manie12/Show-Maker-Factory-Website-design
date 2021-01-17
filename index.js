5a821994f2860839e59eab0acf5aa4ba66c2f50b
Jacob Runo <jacobkip254@gmail.com>
2021-01-17 02:27:35 -0800

    designed the whole website

---
+++ index.js
@@ -0,0 +1,12 @@
+const header = document.querySelector('.image__menu');
+const headerhide = document.querySelector('.header__hide');
+const headerreveal = document.querySelector('.header__wooten');
+const body = document.querySelector('.body1')
+
+
+
+header.addEventListener('click', () => {
+    headerreveal.classList.toggle('header__hide')
+
+
+})
